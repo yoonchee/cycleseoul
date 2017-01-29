1ccb4cf4714c9c990c8dc842c5f99220bac3b654
Chee-Hyung Yoon <yoon@virect.com>
2017-01-29 14:21:21 +0900

    Add client-side injection of name, length, and elevation.

--- imports/ui/Route.jsx
+++ imports/ui/Route.jsx
@@ -4,18 +4,35 @@ import { Routes } from '../api/routes.js';
 
 // Task component - represents a single todo item
 export default class Route extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      length: 0,
+      elevation: 0,
+      description: '',
+    };
+  }
+
   render() {
-  	return (
-  	  <li className='route'>
-  	    <div>{this.props.route.name}</div>
-  	    <div id={this.props.route._id} className='map'></div>
-  	  </li>
-  	);
+    return (
+      <li className='route'>
+        <h2>{this.state.name}</h2>
+        <dl className='stats'>
+          <dt className='length'>Length</dt>
+          <dd>{(this.state.length / 1000).toFixed(0)}km</dd>
+          <dt className='elevation'>Elevation</dt>
+          <dd>{this.state.elevation.toFixed(0)}m</dd>
+        </dl>
+        <div className='description'>{this.state.description}</div>
+        <div id={this.props.route._id} className='map'></div>
+      </li>
+    );
   }
 
   componentDidMount() {
-  	const access_token = 'pk.eyJ1IjoieW9vbmNoZWUiLCJhIjoiY2l0MmdzZGd1MHNwaTJ1cXA1Z3k3M3JxeSJ9.Rwk0oJ53OFJlzO7iF_r7Mg';
-  	const map = L.map(this.props.route._id, {
+    const access_token = 'pk.eyJ1IjoieW9vbmNoZWUiLCJhIjoiY2l0MmdzZGd1MHNwaTJ1cXA1Z3k3M3JxeSJ9.Rwk0oJ53OFJlzO7iF_r7Mg';
+    const map = L.map(this.props.route._id, {
       doubleClickZoom: false
     });
 
@@ -24,8 +41,15 @@ export default class Route extends Component {
       {detectRetina: true}
     ).addTo(map);
 
-    const gpxLayer = new L.GPX(this.props.route.gpx, {async: true}).on('loaded', function(e) {
-      map.fitBounds(e.target.getBounds());
+    const that = this;
+    new L.GPX(this.props.route.gpx, {async: true}).on('loaded', function(e) {
+      const gpx = e.target;
+      that.setState({
+        name: gpx.get_name(),
+        length: gpx.get_distance(),
+        elevation: gpx.get_elevation_gain(),
+      });
+      map.fitBounds(gpx.getBounds());
     }).addTo(map);
   }
 }
