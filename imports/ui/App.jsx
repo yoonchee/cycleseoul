bc9412683e9a7fe4e49585d2a7fd67f8549ad445
Chee-Hyung Yoon <yoon@virect.com>
2017-01-30 15:57:36 +0900

    Add user accounts support.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Grid, Row, Col, ListGroup, Jumbotron } from 'react-bootstrap';
 
 import { Routes } from '../api/routes.js';
 
 import Route from './Route.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 // App component - represents the whole app
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   handleSubmit(event) {
     event.preventDefault();
 
@@ -28,6 +24,9 @@ class App extends Component {
       Routes.insert({
         gpx: reader.result,
         description: description,
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username, // username of logged in user
       });
       console.log("Succesfully inserted a route.");
     }
@@ -46,6 +45,8 @@ class App extends Component {
         <Row>
           <Col md={12}>
             <header>
+              <AccountsUIWrapper />
+
               <Jumbotron>
                 <h1>Cycling in Seoul</h1>
                 <p>
@@ -57,21 +58,23 @@ class App extends Component {
                 </p>
               </Jumbotron>
 
-              <form encType="multipart/form-data" method="post" className="new-route"
-                    onSubmit={this.handleSubmit.bind(this)} >
-                <input
-                  type="file"
-                  ref="fileInput"
-                  placeholder="Add GPX file"
-                />
-                <textarea
-                  ref="textInput"
-                  rows="4"
-                  cols="70"
-                  placeholder="Add description"
-                />
-                <input type="submit" />
-              </form>
+              { this.props.currentUser ?
+                <form encType="multipart/form-data" method="post" className="new-route"
+                      onSubmit={this.handleSubmit.bind(this)} >
+                  <input
+                    type="file"
+                    ref="fileInput"
+                    placeholder="Add GPX file"
+                  />
+                  <textarea
+                    ref="textInput"
+                    rows="4"
+                    cols="70"
+                    placeholder="Add description"
+                  />
+                  <input type="submit" />
+                </form> : ''
+              }
             </header>
 
             <h2>Top {this.props.routesCount} Cycling Routes in Seoul</h2>
@@ -89,11 +92,13 @@ class App extends Component {
 App.propTypes = {
   routes: PropTypes.array.isRequired,
   routesCount: PropTypes.number.isRequired,
+  currnetUser: PropTypes.object,
 };
 
 export default createContainer(() => {
   return {
     routes: Routes.find().fetch(),
     routesCount: Routes.find().count(),
+    currentUser: Meteor.user(),
   };
 }, App);
